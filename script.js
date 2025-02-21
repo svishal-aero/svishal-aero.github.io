70b86fb65397c468807d4fb2c58f50bfe635d195
svishal-aero <svishal.aero@gmail.com>
2025-02-21 18:53:57 -0500

    Updated the website

---
+++ script.js
@@ -0,0 +1,21 @@
+function adjustImageAndText()
+{
+  const image = document.getElementById('profile-picture');
+  const text = document.getElementById('intro-text');
+  const aspectRatio = image.naturalHeight / image.naturalWidth;
+
+  let currWidth = 0;
+  image.style.width = `${currWidth}px`;
+  image.style.display = 'block';
+
+  while(true)
+  {
+    currWidth += 0.1;
+    const currHeight = currWidth * aspectRatio;
+    image.style.width = `${currWidth}px`;
+    image.style.height = `${currHeight}px`;
+    if (currHeight >= text.clientHeight) break;
+  }
+}
+
+window.onload = adjustImageAndText;
